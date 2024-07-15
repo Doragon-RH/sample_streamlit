a7185bedc282570f4338c31bc89b00554c99302f
Doragon-RH <r216af@icloud.com>
2024-07-16 00:42:37 +0900

    レイアウト系のk－小戸

--- main.py
+++ main.py
@@ -39,3 +39,37 @@ st.write("You selected:", options)
 
 age = st.slider("何歳？", min_value=0, max_value=130, value=25)
 st.write(age)
+
+from datetime import datetime,date,time
+
+interval = st.slider(
+    "計画期間は？",
+    value = (datetime(2019,1,1,9,30),datetime(2020,1,1,9,30)),
+    format="MM/DD/YY - hh:mm",
+)
+color = st.select_slider(
+    "色を選んでね",
+    options = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+)
+
+title = st.text_input("お名前は？", value="ななしのごんべえ")
+
+number = st.number_input("数字を入れてね",
+                         min_value=1.0, max_value=10.0, value=5.0, step=0.1)
+
+d = st.date_input("誕生日は？", date(2019,7,6))
+
+alarm = st.time_input("アラームセット", time(8,45))
+uploaded_file = st.file_uploader("ファイル選択")
+
+#サイドバーに表示
+add_selectbox = st.sliderbar.selectbox("連絡方法は？",
+                                       ("Email", "Home phone", "Mobile phone"))
+
+col1, col2 = st.beta_columns([1,2])
+with col1:
+    st.header("A cat")
+    st.image("https://statuc.streamlit.io/examples/cat.jpg", use_column_width=True)
+with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg", use_column_width=True)
